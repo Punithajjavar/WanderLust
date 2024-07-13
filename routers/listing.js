99919bd5d3bb0ab2546d0cbb4b58ec9e60d8dbd9
punith jawar <punithajjavar@gmail.com>
2024-07-13 05:53:14 -1200

    saved

--- routers/listing.js
+++ routers/listing.js
@@ -1,8 +1,8 @@
 const express =require("express");
 const router =express.Router();
-const wrapAsync =require("./utils/wrapAsync.js");
-const ExpressError =require("./utils/ExpressError.js");
-const {listingSchema,reviewSchema}=require("./schema.js");
+const wrapAsync =require("../utils/wrapAsync.js");
+const ExpressError =require("../utils/ExpressError.js");
+const {listingSchema,reviewSchema}=require("../schema.js");
 const Listing=require("../models/listing.js");
 
 
